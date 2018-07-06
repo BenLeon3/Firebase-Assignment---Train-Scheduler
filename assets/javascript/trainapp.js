87820c71bbf0bf57bbc068475ea0056527927dff
Benjamin Leon <benleon3@gmail.com>
2018-07-05 20:48:06 -0700

    innitial commit

---
+++ assets/javascript/trainapp.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBE0qZeK97Za61nbgLYcZfxsU9giknYdqs",
+    authDomain: "train-schedule-hw-fd4c5.firebaseapp.com",
+    databaseURL: "https://train-schedule-hw-fd4c5.firebaseio.com",
+    projectId: "train-schedule-hw-fd4c5",
+    storageBucket: "train-schedule-hw-fd4c5.appspot.com",
+    messagingSenderId: "229170455730"
+  };
+  firebase.initializeApp(config);
+</script>
